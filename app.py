b3ebb36760d1b38ca05673c1ac4b46cd58fc28f5
Pauli Varelius <pauli@matin-29.local>
2021-03-23 18:55:21 +0200

    serving frontend from static folder seems to work

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from multiprocessing import Value, Lock
 import json
 import datetime
 import adafruit_dht
-from flask import Flask, jsonify, render_template, send_from_directory
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS, cross_origin
 
 dhtSensor = adafruit_dht.DHT22(board.D4)
 
-app = Flask(__name__, static_url_path='/')
+app = Flask(__name__, static_folder='./static/static/', template_folder='./static')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -58,7 +58,7 @@ def sensor():
 
 @app.route("/")
 def hello():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 
 @app.route('/api/sensors/house/current_data')
